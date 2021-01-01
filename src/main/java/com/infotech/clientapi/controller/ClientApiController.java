280e4b5dad9dc574f98554eceab6858b7f941fed
vijayproject1 <vijayk.bura@gmail.com>
2021-01-01 16:59:57 +0530

    Revert "Added mongodb"
    This reverts commit 48473410

--- src/main/java/com/infotech/clientapi/controller/ClientApiController.java
+++ src/main/java/com/infotech/clientapi/controller/ClientApiController.java
@@ -1,27 +1,18 @@
 package com.infotech.clientapi.controller;
 
 
-import com.infotech.clientapi.dao.UserRepository;
-import com.infotech.clientapi.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RefreshScope
-@RequestMapping("/user")
+@RequestMapping("/client")
 public class ClientApiController {
-
-    @Autowired
-    UserRepository userRepository;
 
 
     private static final Logger logger = LoggerFactory.getLogger(ClientApiController.class);
@@ -33,19 +24,5 @@ public class ClientApiController {
     public String getUserName() {
         logger.info("sample Request Received");
         return "Hi "+ clientValue;
-    }
-
-
-    @GetMapping("/{user}/{pass}")
-    public User addUser(@PathVariable("user") String user,
-                        @PathVariable("pass") String pass ) {
-        logger.info("User Request Received {} ,{} ", user, pass);
-        return userRepository.save(new User(user,pass));
-    }
-
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        logger.info("GET User Request Received ");
-        return userRepository.findAll();
     }
 }
--- src/main/java/com/infotech/clientapi/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.infotech.clientapi.dao;
-
-
-import com.infotech.clientapi.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-}
